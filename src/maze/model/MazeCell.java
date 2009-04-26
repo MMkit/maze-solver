48948d806ca48fa2d48e75967d4aada97eae5bdc
lukelast <lukelast@193d5b7c-3166-11de-9e80-b52b74bdfc52>
2009-04-26 02:35:19 +0000

    Added a code formatter for the eclipse IDE and changed all tabs to spaces so the code displays better in the google code browser.

--- src/maze/model/MazeCell.java
+++ src/maze/model/MazeCell.java
@@ -8,43 +8,43 @@ import java.awt.Point;
  */
 public final class MazeCell
 {
-	private final int x;
-	private final int y;
+   private final int x;
+   private final int y;
 
-	public MazeCell( int xCoordinate, int yCoordinate )
-	{
-		if ( xCoordinate <= 0 )
-			throw new IllegalArgumentException( "X coordinate must be greater than 0." );
-		if ( yCoordinate <= 0 )
-			throw new IllegalArgumentException( "Y coordinate must be greater than 0." );
+   public MazeCell( int xCoordinate, int yCoordinate )
+   {
+      if ( xCoordinate <= 0 )
+         throw new IllegalArgumentException( "X coordinate must be greater than 0." );
+      if ( yCoordinate <= 0 )
+         throw new IllegalArgumentException( "Y coordinate must be greater than 0." );
 
-		this.x = xCoordinate;
-		this.y = yCoordinate;
-	}
+      this.x = xCoordinate;
+      this.y = yCoordinate;
+   }
 
-	public int getX()
-	{
-		return this.x;
-	}
+   public int getX()
+   {
+      return this.x;
+   }
 
-	public int getY()
-	{
-		return this.y;
-	}
+   public int getY()
+   {
+      return this.y;
+   }
 
-	public int getXZeroBased()
-	{
-		return this.x - 1;
-	}
+   public int getXZeroBased()
+   {
+      return this.x - 1;
+   }
 
-	public int getYZeroBased()
-	{
-		return this.y - 1;
-	}
+   public int getYZeroBased()
+   {
+      return this.y - 1;
+   }
 
-	public Point getPoint()
-	{
-		return new Point( this.x, this.y );
-	}
+   public Point getPoint()
+   {
+      return new Point( this.x, this.y );
+   }
 
 }
