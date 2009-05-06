28f71a1cc16e68a80f5d610ced71646364095dbb
eNVee569 <eNVee569@193d5b7c-3166-11de-9e80-b52b74bdfc52>
2009-05-06 17:14:02 +0000

    added Menu to Primary Frame. I think thats where I should put it. Still need to expand some more on some Items.

--- src/maze/gui/PrimaryFrame.java
+++ src/maze/gui/PrimaryFrame.java
@@ -1,6 +1,9 @@
 package maze.gui;
 
 import javax.swing.JFrame;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
 
 /**
  * This is the primary frame for the application.
@@ -12,6 +15,67 @@ public final class PrimaryFrame extends JFrame
     */
    public PrimaryFrame()
    {
+      // menu bar
+      JMenuBar menuBar = new JMenuBar();
+       
+      //File item
+      JMenu fileMenu = new JMenu("File");
+      menuBar.add(fileMenu);
+      
+      // Save
+      JMenuItem fileSave = new JMenuItem("Save");
+      fileMenu.add(fileSave);
+
+      // load
+      JMenuItem fileLoad = new JMenuItem("Load");
+      fileMenu.add(fileLoad);
+      
+      // exit
+      JMenuItem fileExit = new JMenuItem("Exit");
+      fileMenu.add(fileExit);
+      
+      //Maze Option item
+      JMenu mazeMenu = new JMenu("Maze Options");
+      menuBar.add(mazeMenu);
+
+      // Load Maze
+      JMenuItem mazeLoad = new JMenuItem("Load Maze");
+      mazeMenu.add(mazeLoad);
+
+      // Save Maze
+      JMenuItem mazeSave = new JMenuItem("Save Maze");
+      mazeMenu.add(mazeSave);
+      
+      // maze templetes
+      JMenuItem mazeTemp = new JMenuItem("Maze Templates");
+      mazeMenu.add(mazeTemp);
+       
+      // Mouse options
+      JMenu mouseMenu = new JMenu("Mouse Options");
+      menuBar.add(mouseMenu);
+      
+      // Load AI
+      JMenuItem mouseLoad = new JMenuItem("Load AI");
+      mouseMenu.add(mouseLoad);
+      
+      // Save AI
+      JMenuItem mouseSave = new JMenuItem("Save AI");
+      mouseMenu.add(mouseSave);
+      
+      // Algorithms
+      JMenuItem mouseAlgorithms = new JMenuItem("AI Algrithms");
+      mouseMenu.add(mouseAlgorithms);
+      
+      // Mouse Speed
+      JMenuItem mouseSpeed = new JMenuItem("Mouse Speed");
+      mouseMenu.add(mouseSpeed);
+      
+      // Display Settings
+      JMenuItem mouseDisplay = new JMenuItem("Display Settings");
+      mouseMenu.add(mouseDisplay);
+      
+      this.setJMenuBar(menuBar);
+       
       this.setSize( 1000, 750 );
 
       this.add( new MazeView() );
