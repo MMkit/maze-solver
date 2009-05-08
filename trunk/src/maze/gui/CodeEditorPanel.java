b9748ae100c6ce165b23b919caca09ceaac4ca76
lukelast <lukelast@193d5b7c-3166-11de-9e80-b52b74bdfc52>
2009-05-08 22:43:47 +0000

    Flesh out the AI script editor GUI.

--- trunk/src/maze/gui/CodeEditorPanel.java
+++ trunk/src/maze/gui/CodeEditorPanel.java
@@ -7,8 +7,12 @@ import java.awt.event.KeyEvent;
 
 import javax.swing.AbstractAction;
 import javax.swing.Action;
+import javax.swing.JEditorPane;
 import javax.swing.JMenuItem;
 import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.JSplitPane;
+import javax.swing.JTabbedPane;
 import javax.swing.KeyStroke;
 
 import maze.model.Maze;
@@ -19,60 +23,90 @@ import org.python.core.PyList;
 import org.python.core.PyObject;
 import org.python.util.PythonInterpreter;
 
-public class CodeEditorPanel extends RTextScrollPane
+/**
+ * Creates a robot AI script editor that allows the creation of Python scripts
+ * to control the AI robot.
+ * @author Luke Last
+ */
+public class CodeEditorPanel extends JSplitPane
 {
+
    private final RSyntaxTextArea textArea = new RSyntaxTextArea();
    private final String[] excludedStrings = new String[]
    {
       "clone", "finalize", "getClass", "hashCode", "notify", "notifyAll", "wait",
    };
 
+   /**
+    * Constructor.
+    */
    public CodeEditorPanel()
    {
-      //final RTextScrollPane scrollPane = new RTextScrollPane();
-      super.setViewportView( this.textArea );
-      super.setLineNumbersEnabled( true );
-      //this.add( scrollPane );
+      //Set up the scroll pane that holds the code editor.
+      final RTextScrollPane scrollPane = new RTextScrollPane();
+      scrollPane.setViewportView(this.textArea);
+      scrollPane.setLineNumbersEnabled(true);
 
-      this.textArea.setMarkOccurrences( true );
-      this.textArea.setTextAntiAliasHint( "VALUE_TEXT_ANTIALIAS_ON" );
-      this.textArea.setSyntaxEditingStyle( RSyntaxTextArea.SYNTAX_STYLE_PYTHON );
+      //Set up the documentation browser.
+      final JEditorPane docPane = new JEditorPane();
+      docPane.setEditable(false);
+      docPane.setContentType("text/html");
+      docPane.setText("<html><h2>This page will contain documentation</h2><p>blah blah</p></html>");
 
-      JMenuItem menuItem = new JMenuItem( "Analyze Object" );
+      //Set up the documentation tabs.
+      final JTabbedPane docTabs = new JTabbedPane();
+
+      docTabs.add("Code Information", new CodeInformationPanel());
+      docTabs.add("API Documentation", docPane);
+
+      //Set up the code editor.
+      this.textArea.setMarkOccurrences(true);
+      this.textArea.setTextAntiAliasHint("VALUE_TEXT_ANTIALIAS_ON");
+      this.textArea.setSyntaxEditingStyle(RSyntaxTextArea.SYNTAX_STYLE_PYTHON);
+
+      //Set up split pane.
+      this.setLeftComponent(scrollPane);
+      this.setRightComponent(docTabs);
+      //TODO set divider location.
+
+      JMenuItem menuItem = new JMenuItem("Analyze Object");
       this.textArea.getPopupMenu().addSeparator();
-      this.textArea.getPopupMenu().add( menuItem );
-      menuItem.addActionListener( new ActionListener()
+      this.textArea.getPopupMenu().add(menuItem);
+      menuItem.addActionListener(new ActionListener()
       {
+
          @Override
-         public void actionPerformed( ActionEvent e )
+         public void actionPerformed(ActionEvent e)
          {
             analyzeSelection();
          }
-      } );
+      });
 
-      JMenuItem evalMenuItem = new JMenuItem( "Evaluate Variable" );
-      this.textArea.getPopupMenu().add( evalMenuItem );
-      evalMenuItem.addActionListener( new ActionListener()
+      JMenuItem evalMenuItem = new JMenuItem("Evaluate Variable");
+      this.textArea.getPopupMenu().add(evalMenuItem);
+      evalMenuItem.addActionListener(new ActionListener()
       {
+
          @Override
-         public void actionPerformed( ActionEvent e )
+         public void actionPerformed(ActionEvent e)
          {
             evaluateSelection();
          }
-      } );
+      });
 
       Action action = new AbstractAction()
       {
+
          @Override
-         public void actionPerformed( ActionEvent e )
+         public void actionPerformed(ActionEvent e)
          {
             executeScript();
          }
       };
 
-      this.textArea.getKeymap().addActionForKeyStroke( KeyStroke.getKeyStroke( KeyEvent.VK_ENTER,
-                                                                               InputEvent.SHIFT_DOWN_MASK ),
-                                                       action );
+      this.textArea.getKeymap().addActionForKeyStroke(KeyStroke.getKeyStroke(KeyEvent.VK_ENTER,
+                                                                             InputEvent.SHIFT_DOWN_MASK),
+                                                      action);
    }
 
    private void evaluateSelection()
@@ -80,16 +114,16 @@ public class CodeEditorPanel extends RTextScrollPane
       try
       {
          PythonInterpreter interp = this.getInitializedInterpreter();
-         PyObject obj = interp.eval( this.textArea.getSelectedText() );
-         JOptionPane.showMessageDialog( this, obj.toString() );
+         PyObject obj = interp.eval(this.textArea.getSelectedText());
+         JOptionPane.showMessageDialog(this, obj.toString());
       }
-      catch ( Exception e )
+      catch (Exception e)
       {
-         JOptionPane.showMessageDialog( this,
-                                        e.toString(),
-                                        "Error Evaluating Variable!",
-                                        JOptionPane.ERROR_MESSAGE,
-                                        null );
+         JOptionPane.showMessageDialog(this,
+                                       e.toString(),
+                                       "Error Evaluating Variable!",
+                                       JOptionPane.ERROR_MESSAGE,
+                                       null);
       }
    }
 
@@ -98,61 +132,58 @@ public class CodeEditorPanel extends RTextScrollPane
       try
       {
          PythonInterpreter interp = this.getInitializedInterpreter();
-         PyObject selectedObj = interp.get( this.textArea.getSelectedText() );
-         PyObject obj = interp.eval( "dir(" + this.textArea.getSelectedText() + ")" );
+         PyObject selectedObj = interp.get(this.textArea.getSelectedText());
+         PyObject obj = interp.eval("dir(" + this.textArea.getSelectedText() + ")");
          PyList pyList = (PyList) obj;
          StringBuilder sb = new StringBuilder();
-         for ( Object o : pyList )
+         for (Object o : pyList)
          {
             final String s = o.toString();
             boolean match = false;
-            for ( String exclude : excludedStrings )
+            for (String exclude : excludedStrings)
             {
-               if ( exclude.equals( s ) )
+               if (exclude.equals(s))
                {
                   match = true;
                   break;
                }
             }
-            if ( match == false && ( s.startsWith( "__" ) == false || s.equals( "__doc__" ) ) )
+            if (match == false && (s.startsWith("__") == false || s.equals("__doc__")))
             {
-               sb.append( o.toString() );
-               sb.append( '\n' );
+               sb.append(o.toString());
+               sb.append('\n');
             }
          }
          String title = "Available Methods";
-         if ( selectedObj != null )
+         if (selectedObj != null)
          {
             title = selectedObj.getType().toString();
          }
 
-         JOptionPane.showMessageDialog( this,
-                                        sb.toString(),
-                                        title,
-                                        JOptionPane.INFORMATION_MESSAGE,
-                                        null );
+         JOptionPane.showMessageDialog(this,
+                                       sb.toString(),
+                                       title,
+                                       JOptionPane.INFORMATION_MESSAGE,
+                                       null);
       }
-      catch ( Exception e2 )
+      catch (Exception e2)
       {
-         JOptionPane.showMessageDialog( this,
-                                        e2.toString(),
-                                        "Error analyzing variable",
-                                        JOptionPane.ERROR_MESSAGE,
-                                        null );
+         JOptionPane.showMessageDialog(this,
+                                       e2.toString(),
+                                       "Error analyzing variable",
+                                       JOptionPane.ERROR_MESSAGE,
+                                       null);
       }
    }
 
    private PythonInterpreter getInitializedInterpreter()
    {
       final PythonInterpreter interp = new PythonInterpreter();
-      interp.exec( "from maze.ai import RobotStep" );
-      interp.exec( "from maze.model import WallDirection, MazeCell" );
-      interp.set( "maze", new Maze() );
+      interp.exec("from maze.ai import RobotStep");
+      interp.exec("from maze.model import WallDirection, MazeCell");
+      interp.set("maze", new Maze());
 
-      SuperClass dude = new Extender();
-      interp.set( "test", dude );
-
-      interp.exec( this.textArea.getText() );
+      interp.exec(this.textArea.getText());
       return interp;
    }
 
@@ -162,39 +193,24 @@ public class CodeEditorPanel extends RTextScrollPane
       {
          final PythonInterpreter interp = this.getInitializedInterpreter();
 
-         //interp.exec( "y = 5" );
-         PyObject obj = interp.get( "nextStep" );
-         JOptionPane.showMessageDialog( this, obj.__call__() );
+         PyObject obj = interp.get("nextStep");
+         JOptionPane.showMessageDialog(this, obj.__call__());
 
-         System.out.println( obj.getType() );
-         System.out.println( obj.toString() );
-         System.out.println( obj.__call__() );
-         System.out.println( obj.__call__() );
-         System.out.println( obj.__call__() );
+         System.out.println(obj.getType());
+         System.out.println(obj.toString());
+         System.out.println(obj.__call__());
       }
-      catch ( Exception e )
+      catch (Exception e)
       {
-         JOptionPane.showMessageDialog( this,
-                                        e.toString(),
-                                        "Script Error",
-                                        JOptionPane.ERROR_MESSAGE,
-                                        null );
+         JOptionPane.showMessageDialog(this,
+                                       e.toString(),
+                                       "Script Error",
+                                       JOptionPane.ERROR_MESSAGE,
+                                       null);
       }
    }
 
-   static abstract class SuperClass
+   private final class CodeInformationPanel extends JPanel
    {
-      public String superClass()
-      {
-         return "Hello from abstract super class.";
-      }
-   }
-
-   public static class Extender extends SuperClass
-   {
-      public String writer()
-      {
-         return "Hello from class you shouldn't have access to.";
-      }
    }
 }
