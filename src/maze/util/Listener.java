253d339ff9a3738db9ec2ab4fb8aca2a2d4a6f91
lukelast <lukelast@193d5b7c-3166-11de-9e80-b52b74bdfc52>
2009-06-03 15:11:16 +0000

    Added comments.

--- src/maze/util/Listener.java
+++ src/maze/util/Listener.java
@@ -3,6 +3,8 @@ package maze.util;
 /**
  * This Listener interface allows the creation of Listener classes that can
  * register themselves with a ListenerSubject.
+ * @see ListenerSubject
+ * @version 2009-05-30
  * @author Luke Last
  * @param <T> The type of the event object that is passed in when the event is
  *           fired. This type should match the defined generic type of the
@@ -13,7 +15,8 @@ public interface Listener<T>
    /**
     * This is called when the ListenerSubject wants to notify the registered
     * Listeners.
-    * @param event The event object.
+    * @param event The event object. It can be null so make sure and check for
+    *           that.
     */
    public void eventFired(T event);
 }
