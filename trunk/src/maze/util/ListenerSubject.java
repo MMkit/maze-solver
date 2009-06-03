d83f5224c4bd425fd8fd6c7e73418d3757df4d86
lukelast <lukelast@193d5b7c-3166-11de-9e80-b52b74bdfc52>
2009-06-03 15:11:16 +0000

    Added comments.

--- trunk/src/maze/util/ListenerSubject.java
+++ trunk/src/maze/util/ListenerSubject.java
@@ -3,8 +3,16 @@ package maze.util;
 import java.awt.EventQueue;
 
 /**
+ * Implements the observer design pattern. A ListenerSubject can have Listeners
+ * registered with it and then notify those Listeners when something happens.
+ * This class can be extended or instances of it can be created to allow a class
+ * to have multiple events that can be registered with.
  * @author Luke Last
- * @param <T> The type of the event objects to be used by the Listeners.
+ * @see Listener
+ * @version 2009-05-30
+ * @param <T> The type of the event objects to be used by the Listeners. Objects
+ *           of this type are passed to each registered Listener when they are
+ *           notified.
  */
 public class ListenerSubject<T>
 {
@@ -15,7 +23,7 @@ public class ListenerSubject<T>
    private Listener<T> listener;
 
    /**
-    * Stores listeners that should be notified in a delayed manner.
+    * Stores listeners that should be notified in a delayed manner. Can be null.
     */
    private Listener<T> delayedListener;
 
@@ -26,11 +34,18 @@ public class ListenerSubject<T>
    private volatile boolean pendingNotify = false;
 
    /**
-    * Notifies all registered listeners.
+    * This is created when needed.
+    */
+   private DelayedNotifier delayedNotifier;
+
+   /**
+    * Notifies all registered listeners. Normal listeners are notified
+    * Immediately, delayed listeners are notified with a new task on the event
+    * queue.
     * @param event The event object that should be passed to each registered
     *           Listener.
     */
-   public void notifyListeners(final T event)
+   public final void notifyListeners(final T event)
    {
       final Listener<T> copy = this.listener;
       if (copy != null)
@@ -40,19 +55,50 @@ public class ListenerSubject<T>
       if (this.delayedListener != null && this.pendingNotify == false)
       {
          this.pendingNotify = true;
-         EventQueue.invokeLater(new Runnable()
+         EventQueue.invokeLater(this.getDelayedNotifier(event));
+      }
+   }
+
+   /**
+    * This allows us to only create a new instance once and then reuse it for
+    * better performance.
+    * @param eventObject This event object is given to the notifier which it
+    *           then passes to the listeners.
+    * @return The one and only instance.
+    */
+   private DelayedNotifier getDelayedNotifier(T eventObject)
+   {
+      if (this.delayedNotifier == null)
+      {
+         this.delayedNotifier = new DelayedNotifier();
+      }
+      this.delayedNotifier.setEventObject(eventObject);
+      return this.delayedNotifier;
+   }
+
+   /**
+    * A <code>Runnable</code> which is invoked later on the event queue. We
+    * create a special class for this and then instantiate it only once for
+    * better performance.
+    */
+   private final class DelayedNotifier implements Runnable
+   {
+      private T eventObject;
+
+      public void setEventObject(T eventObject)
+      {
+         this.eventObject = eventObject;
+      }
+
+      @Override
+      public void run()
+      {
+         pendingNotify = false;
+         final Listener<T> delayedCopy = delayedListener;
+         if (delayedCopy != null)
          {
-            @Override
-            public void run()
-            {
-               pendingNotify = false;
-               final Listener<T> delayedCopy = delayedListener;
-               if (delayedCopy != null)
-               {
-                  delayedCopy.eventFired(event);
-               }
-            }
-         });
+            delayedCopy.eventFired(this.eventObject);
+         }
       }
    }
 
@@ -61,7 +107,7 @@ public class ListenerSubject<T>
     * to see if it already is registered.
     * @param listener The Listener you would like to add.
     */
-   public synchronized void addListener(final Listener<T> listener)
+   public final synchronized void addListener(final Listener<T> listener)
    {
       if (listener != null)
       {
@@ -79,7 +125,7 @@ public class ListenerSubject<T>
    /**
     * @param listener
     */
-   public synchronized void addDelayedListener(final Listener<T> listener)
+   public final synchronized void addDelayedListener(final Listener<T> listener)
    {
       if (listener != null)
       {
@@ -95,10 +141,11 @@ public class ListenerSubject<T>
    }
 
    /**
-    * Unregisters a Listener with this subject.
+    * Unregisters a Listener with this subject. If the given listener is null or
+    * not already registered no changes are made.
     * @param listener The Listener you want to remove.
     */
-   public synchronized void removeListener(final Listener<T> listener)
+   public final synchronized void removeListener(final Listener<T> listener)
    {
       if (this.listener != null && listener != null)
       {
@@ -106,7 +153,7 @@ public class ListenerSubject<T>
          {
             this.listener = null;
          }
-         else if (this.listener instanceof ListenerMulticaster)
+         else if (this.listener instanceof ListenerMulticaster<?>)
          {
             final ListenerMulticaster<T> multicaster = (ListenerMulticaster<T>) this.listener;
             this.listener = multicaster.remove(listener);
@@ -114,19 +161,22 @@ public class ListenerSubject<T>
       }
    }
 
-   public synchronized void removeAllListeners()
+   /**
+    * Removes all listeners registered with this subject.
+    */
+   public final synchronized void removeAllListeners()
    {
       this.listener = null;
    }
 
    /**
     * A utility class used to join 2 Listeners into one Listener interface. It
-    * is used to create a readonly linked list of Listeners.<br />
+    * is used to create a read only linked list of Listeners.<br />
     * Each instance is immutable.
     * @author Luke Last
     * @param <T>
     */
-   private static class ListenerMulticaster<T> implements Listener<T>
+   private final static class ListenerMulticaster<T> implements Listener<T>
    {
       private final Listener<T> a;
       private final Listener<T> b;
@@ -160,28 +210,28 @@ public class ListenerSubject<T>
          }
          final Listener<T> a2;
          final Listener<T> b2;
-         if (this.a instanceof ListenerMulticaster)
+         if (this.a instanceof ListenerMulticaster<?>)
          {
             a2 = ((ListenerMulticaster<T>) this.a).remove(toRemove);
-            if (this.b instanceof ListenerMulticaster)
+            if (this.b instanceof ListenerMulticaster<?>)
             {
                b2 = ((ListenerMulticaster<T>) this.b).remove(toRemove);
             }
             else
             {
-               b2 = this.b;
+               b2 = this.b; // B is not a multicaster.
             }
          }
          else
-         {
+         { // A is not a multicaster.
             a2 = this.a;
-            if (this.b instanceof ListenerMulticaster)
+            if (this.b instanceof ListenerMulticaster<?>)
             {
                b2 = ((ListenerMulticaster<T>) this.b).remove(toRemove);
             }
             else
             {
-               return this;
+               return this; // A nor B are multicasters.
             }
          }
          return new ListenerMulticaster<T>(a2, b2);
@@ -191,7 +241,7 @@ public class ListenerSubject<T>
        * Passes the event on to children.
        */
       @Override
-      public void eventFired(T event)
+      public void eventFired(final T event)
       {
          if (this.a != null)
          {
